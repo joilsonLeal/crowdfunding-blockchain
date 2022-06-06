8939a2a645483022a78e6e3589e7881279208995
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-06 20:30:35 -0300

    feat: add contribute and modifier

--- contract/contracts/Compaign.sol
+++ contract/contracts/Compaign.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Campaign {
   address public owner;
+  uint256 public minimumContribution;
+  address[] public approvers;
 
-  constructor() {
+  constructor(uint256 minimum) {
     owner = msg.sender;
+    minimumContribution = minimum;
+  }
+
+  modifier contribution() {
+    require(
+      msg.value >= minimumContribution,
+      string.concat(
+        "Value must be greater than ",
+        Strings.toString(minimumContribution),
+        " wei"
+      )
+    );
+    _;
+  }
+
+  function contribute() public payable contribution {
+    approvers.push(msg.sender);
   }
 }
