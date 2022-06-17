12722100f8488128a558d0ba94e236a5ce4d4184
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-17 08:39:51 -0300

    feat: list campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
-import factory from '../ethereum/factory';
+import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
+
+import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    return { campaigns }
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
-    return <div>{this.props.campaigns[0]}</div>
+    return (
+      <div>
+        {this.renderCampaigns()}
+      </div>
+    );
   }
 }
 
