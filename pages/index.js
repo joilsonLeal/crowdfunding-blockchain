89d4995a2db4fcd780733401fc0d285e0cb3fd97
Joilson Leal Bica <joilson_rg@hotmail.com>
2022-06-18 15:24:23 -0300

    feat(index): add navigation

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Card, Button } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import Layout from '../components/Layout';
-
+import Layout from "../components/Layout";
+import { Link } from "../routes";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
@@ -17,7 +17,11 @@ class CampaignIndex extends Component {
     const items = this.props.campaigns.map((address) => {
       return {
         header: address,
-        description: <a>View Campaign</a>,
+        description: (
+          <Link route={`/campaigns/${address}`}>
+            <a>View Campaign</a>
+          </Link>
+        ),
         fluid: true,
       };
     });
@@ -29,12 +33,17 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <h3>Open Campaigns</h3>
-        <Button 
-          floated="right"
-          content = "Create Campaign"
-          icon = "add circle"
-          primary
-        />
+
+        <Link route="/campaigns/new">
+          <a>
+            <Button
+              floated="right"
+              content="Create Campaign"
+              icon="add circle"
+              primary
+            />
+          </a>
+        </Link>
         {this.renderCampaigns()}
       </Layout>
     );
